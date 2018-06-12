aa5d8952d032bac46362c08d7ee467aa24e09f00
civilian <chamatoscar@gmail.com>
2018-06-12 17:28:32 -0500

    Added some stuff(created at, updated at, etc) to posts.show.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,8 +3,37 @@
 @section('title', '| View Post')
 
 @section('content')
+    <div class="row">
+        <div class="col-md-8">
+            <h1>{{ $post->title }}</h1>
+            <p class="lead">{{ $post->body }}</p>  
+        </div>
 
-  <h1>{{ $post->title }}</h1>
-  <p class="lead">{{ $post->body }}</p>
+        <div class="col-md-4">
+            <div class="card card-block bg-faded">
+                <dl class="dl-horizontal">
+                    <dt>Created At:</dt>    
+                    <dd>{{ date('M j, Y h:ia', strtotime($post->created_at)) }}</dd>
+                </dl>
+                
+                <dl class="dl-horizontal">
+                    <dt>Last Updated:</dt>    
+                    <dd>{{ date('M j, Y h:ia', strtotime($post->updated_at)) }}</dd>
+                </dl>
+
+                <hr>
+
+                <div class="row">
+                    <div class="col-sm-6">
+                        {!! Html::linkRoute('posts.edit','Edit',array($post->id), array('class' =>'btn btn-primary btn-block'))!!}
+                    </div>
+                    <div class="col-sm-6">
+                        {!! Html::linkRoute('posts.destroy','Delete',array($post->id), array('class' =>'btn btn-danger btn-block'))!!}
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
 
 @endsection
