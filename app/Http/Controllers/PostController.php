9c40640ce18709ccefa41c998fb5c1a7b0cdfe19
civilian <chamatoscar@gmail.com>
2018-06-14 16:08:30 -0500

    posts.edit is functional now.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -77,7 +77,8 @@ class PostController extends Controller
      */
     public function edit($id)
     {
-        //
+        $post = Post::find($id);
+        return view('posts.edit')->withPost($post);
     }
 
     /**
