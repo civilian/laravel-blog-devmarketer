b5399942198cadd9e440604019945b490268b2ea
civilian <chamatoscar@gmail.com>
2018-06-15 19:13:22 -0500

    Now you can delete posts.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,18 +6,18 @@
     <div class="row">
         <div class="col-md-8">
             <h1>{{ $post->title }}</h1>
-            <p class="lead">{{ $post->body }}</p>  
+            <p class="lead">{{ $post->body }}</p>
         </div>
 
         <div class="col-md-4">
             <div class="card card-block bg-faded">
                 <dl class="dl-horizontal">
-                    <dt>Created At:</dt>    
+                    <dt>Created At:</dt>
                     <dd>{{ date('M j, Y h:ia', strtotime($post->created_at)) }}</dd>
                 </dl>
-                
+
                 <dl class="dl-horizontal">
-                    <dt>Last Updated:</dt>    
+                    <dt>Last Updated:</dt>
                     <dd>{{ date('M j, Y h:ia', strtotime($post->updated_at)) }}</dd>
                 </dl>
 
@@ -28,7 +28,9 @@
                         {!! Html::linkRoute('posts.edit','Edit',array($post->id), array('class' =>'btn btn-primary btn-block'))!!}
                     </div>
                     <div class="col-sm-6">
-                        {!! Html::linkRoute('posts.destroy','Delete',array($post->id), array('class' =>'btn btn-danger btn-block'))!!}
+                        {!! Form::open(['route' => ['posts.destroy', $post->id], 'method' => 'DELETE']) !!}
+                          {{ Form::submit('Delete',['class' => 'btn btn-danger btn-block']) }}
+                        {!! Form::close() !!}
                     </div>
                 </div>
 
