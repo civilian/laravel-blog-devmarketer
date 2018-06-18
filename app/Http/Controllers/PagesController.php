7268be76c8e416b7c9147038bc9a3ad996e96e87
civilian <chamatoscar@gmail.com>
2018-06-18 13:59:14 -0500

    The index now shows only 4 posts.

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,14 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
+
 class PagesController extends Controller {
 
     public function index(){
-        return view('pages.welcome');
+        $posts = Post::orderBy('created_at', 'desc')->limit(4)->get();
+        return view('pages.welcome')->withPosts($posts);
     }
 
     public function contact(){
-        
+
         return view('pages.contact');
     }
 
