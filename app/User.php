18b6eb3944807eb4071c395b090e5a03a498b700
civilian <chamatoscar@gmail.com>
2018-07-04 13:15:29 -0500

    Password reset implemented.

--- app/User.php
+++ app/User.php
@@ -26,4 +26,14 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+    * Send the password reset notification.
+    *
+    * @param string $token
+    * @return void
+    */
+    public function sendPasswordResetNotification($token){
+      $this->notify(new Notifications\MailResetPasswordNotification($token, $this->email));
+    }
 }
---
+++ resources/views/auth/passwords/reset.blade.php
@@ -0,0 +1,35 @@
+@extends('main')
+
+@section('title', '| Forgot My Password')
+
+@section('content')
+
+  <div class="row">
+    <div class="col-md-6 col-md-offset-3">
+      <div class="card">
+        <div class="card-body">
+          <h6 class="card-title">Reset Password</h6>
+          <div class="card-text">
+            {!! Form::open(['url' => 'password/reset', 'method' => 'POST'])!!}
+
+              {{ Form::hidden('token', $token) }}
+
+              {{ Form::label('email', 'Email Address:') }}
+              {{ Form::email('email', $email, ['class' => 'form-control']) }}
+
+              {{ Form::label('password', 'New Password:') }}
+              {{ Form::password('password', ['class' => 'form-control']) }}
+
+              {{ Form::label('password_confirmation', 'Confirm New Password:') }}
+              {{ Form::password('password_confirmation', ['class' => 'form-control']) }}
+
+              {{ Form::submit('Reset Password', ['class' => 'btn btn-primary']) }}
+
+            {!! Form::close() !!}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+@endsection
