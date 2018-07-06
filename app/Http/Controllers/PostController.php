b5e0b5d7329a8253ea0a3d178b368e3a06220aee
civilian <chamatoscar@gmail.com>
2018-07-06 18:19:34 -0500

    Categories added to post crud.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use App\Category;
 use Session;
 use Illuminate\Validation\Rule;
 
@@ -32,7 +33,8 @@ class PostController extends Controller
      */
     public function create()
     {
-        return view('posts.create');
+        $categories = Category::all();
+        return view('posts.create')->withCategories($categories);
     }
 
     /**
@@ -45,9 +47,10 @@ class PostController extends Controller
     {
         // validate the data
         $this->validate($request, array(
-            'title' => 'required|max:255',
-            'slug' => 'required|alpha_dash|min:5|max:255|unique:posts,slug',
-            'body' => 'required',
+            'title'       => 'required|max:255',
+            'slug'        => 'required|alpha_dash|min:5|max:255|unique:posts,slug',
+            'category_id' => 'required|integer',
+            'body'        => 'required',
             ));
 
         // store in the database
@@ -55,6 +58,7 @@ class PostController extends Controller
 
         $post->title = $request->title;
         $post->slug = $request->slug;
+        $post->category_id = $request->category_id;
         $post->body = $request->body;
 
         $post->save();
@@ -86,7 +90,12 @@ class PostController extends Controller
     public function edit($id)
     {
         $post = Post::find($id);
-        return view('posts.edit')->withPost($post);
+        $categories = Category::all();
+        $categoriesAssociativeArray = [];
+        foreach ($categories as $category) {
+            $categoriesAssociativeArray[$category->id] = $category->name;
+        }
+        return view('posts.edit')->withPost($post)->withCategories($categoriesAssociativeArray);
     }
 
     /**
@@ -103,6 +112,7 @@ class PostController extends Controller
           'title' => 'required|max:255',
           'slug' => 'required|alpha_dash|min:5|max:255',
           'slug' => Rule::unique('posts')->ignore($id, 'id'),
+          'category_id' => 'required|integer',
           'body' => 'required'
         ]);
 
@@ -111,6 +121,7 @@ class PostController extends Controller
 
         $post->title = $request->input('title');
         $post->slug = $request->input('slug');
+        $post->category_id = $request->input('category_id');
         $post->body = $request->input('body');
 
         $post->save();
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -16,8 +16,6 @@
 
       @include('partials._messages')
 
-      {{ Auth::check() ? 'Logged In': 'Logged Out'}}
-
       @yield('content')
 
       @include('partials._footer')
